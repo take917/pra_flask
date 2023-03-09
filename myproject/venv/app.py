f86a0158675f1ca856701df0bf0d52bb809296ed
take917 <1tktk2tk3@gmail.com>
2023-03-09 09:14:40 +0900

    make routing

--- myproject/venv/app.py
+++ myproject/venv/app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/page1")
+def page1():
+    return "<p>page1</p>"
+
+@app.route("/page2")
+def page2():
+    return "<p>page2</p>"
+
 if __name__=="__main__":
     app.run()
