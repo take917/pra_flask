c8a0bcc89f7ec6d7fe72befb5819ebb3bc9fa195
take917 <1tktk2tk3@gmail.com>
2023-03-14 09:10:33 +0900

    make a topView

--- myproject/venv/app.py
+++ myproject/venv/app.py
@@ -3,18 +3,10 @@ from flask import render_template
 
 app = Flask(__name__)
 
-list = [
-    "list1",
-    "list2",
-    "list3",
-    "list4",
-    "list5",
-    "list6",
-]
 
-@app.route("/<name>")
-def hello(name):
-    return render_template('index.html',name=name,list=list)
+@app.route("/")
+def top():
+    return render_template('index.html')
 
 if __name__=="__main__":
     app.run()
